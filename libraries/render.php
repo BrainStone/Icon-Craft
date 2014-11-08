9fb8eae309567835119bfc36988356616eff0a3d
BrainStone <yannick@tedworld.de>
2014-11-08 19:37:55 +0100

    Fixed rendering

--- libraries/render.php
+++ libraries/render.php
@@ -7,6 +7,9 @@
     $im = render_block("diamond_block", "diamond_block", "diamond_block");
   }
   
+  $final_size = isset($_GET["size"])? $_GET["size"] : 512;   
+  $size = 2048;
+  
   // Resizing
   $image = imagecreatetruecolor($final_size, $final_size);
   imagealphablending($image, false);
