9a559631e1402537c7b53bdcccfa7c7055dd0309
n3wice <jens.we@web.de>
2014-11-09 23:13:25 +0100

    Aufbau der Webseite
    Ersten Schritte zu Webseite erstellt.

---
+++ index.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+<meta http-equiv=content-type content="text/html; charset=UTF-8">
+
+<script src="includes/js/script.js"></script>
+<script src="includes/js/jquery-latest.min.js"></script>
+
+<link rel="stylesheet" type="text/css" href="includes/css/style.css">
+<link rel="stylesheet" type="text/css" href="includes/css/menu.css">
+
+<title>INDEX</title>
+
+</head>
+<body><br />
+<table border='0' width='95%' align='center' class='index_table'>
+    <tr>
+	<td colspan='3' class='index_header'><img src='http://icon-craft.net/diamond_block/128'>Heder mit text und so .. Spenden Button ? so roter knopf und so(include)</td>
+    </tr>
+    <tr>
+    	<td><a href="#" class="myButton">graphicriver</a></td>
+	<td>2</td>
+	<td>3</td>
+    </tr>
+<tr>
+	<td colspan='3' class='index_footer'>footer</td>
+<tr>
+</table>
+
+</body></html>
