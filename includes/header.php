f7ad54b4095d5eb53a1610e7e0cb09d79374b574
n3wice <jens.we@web.de>
2014-11-11 23:02:15 +0100

    Fixed Header.php

--- includes/header.php
+++ includes/header.php
@@ -1,9 +1,9 @@
 <?php
 print "<table>
 	<tr>
-		<td class="header_left">&nbsp;</td>
-		<td class="header_center">" . translate("header.slogan") . "</td>
-		<td class="header_right">" . print_language_selector(); . "</td>
+		<td class=\"header_left\">&nbsp;</td>
+		<td class=\"header_center\">" . translate("header.slogan") . "</td>
+		<td class=\"header_right\">" . print_language_selector(); . "</td>
 	</tr>
 </table>";
 
