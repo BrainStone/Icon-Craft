957e3747da884d19483b97b5c8b5eb86268398ae
n3wice <jens.we@web.de>
2014-11-11 22:59:09 +0100

    Anpassungen Webseite
    style & header

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,10 @@
 <?php
-print "<br /><br /><br /><br /><br />" . translate("header.slogan");
+print "<table>
+	<tr>
+		<td class="header_left">&nbsp;</td>
+		<td class="header_center">" . translate("header.slogan") . "</td>
+		<td class="header_right">" . print_language_selector(); . "</td>
+	</tr>
+</table>";
 
 ?>
