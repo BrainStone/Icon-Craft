3297d4f6298ea98880032f7d831e6357a57ddc28
BrainStone <yannick@tedworld.de>
2014-11-13 23:07:12 +0100

    Fixing

--- includes/uptime.php
+++ includes/uptime.php
@@ -6,7 +6,7 @@
   function percentage($percentage) {
     $percentage_circle = (($percentage >= 0) ? (1.0 - round($percentage, 4)) : 1.0) * 180.0 * M_PI;
     $percentage_display = ($percentage >= 0) ? round($percentage * 100, 2) . " %" : "n/a";
-    $color = get_color_from_gradient($percentage * 100, array(0 => 0x781003, 80 => 0xE31820, 90 => 0xF76820, 95 => 0xD7D820, 99 => 0x2C9F1E, 100 => 0x128007));
+    $color = "#" . dechex(get_color_from_gradient($percentage * 100, array(0 => 0x781003, 80 => 0xE31820, 90 => 0xF76820, 95 => 0xD7D820, 99 => 0x2C9F1E, 100 => 0x128007)));
     
     echo "<div class=\"percent\" data-pct=\"$percentage_display\"><svg viewPort=\"0 0 100 100\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">
 <circle r=\"45\" cx=\"50\" cy=\"50\"></circle><circle r=\"45\" cx=\"50\" cy=\"50\" stroke-dashoffset=\"$percentage_circle\" stroke=\"$color\"></circle>
