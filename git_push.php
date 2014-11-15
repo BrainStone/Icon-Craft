f8e43e3e54a3d207b32345db0cff7dda7db09df9
BrainStone <yannick@tedworld.de>
2014-11-15 22:19:50 +0100

    Implemented automatic update on push to GitHub

---
+++ git_push.php
@@ -0,0 +1,31 @@
+<?php
+  require_once("libraries/passwords.php");
+  
+  header("Content-type: text/html;charset=utf-8");
+  
+  if($headers["User-Agent"] == "GitHub-Hookshot/" . $passwords->github->hookshot) {
+    $request_body = file_get_contents('php://input');
+    $data = json_decode($request_body);
+    
+    if(($headers["X-Github-Event"] == "push") && ($data->ref == "refs/heads/master")) {
+      $algorithm = explode("=", $headers["X-Hub-Signature"])[0];
+      
+      if($headers["X-Hub-Signature"] == ("$algorithm=" . hash_hmac($algorithm, $request_body, $passwords->github->secret))) {
+        passthru("git reset --hard && git clean -f && git pull");
+      }
+    }
+  } else {
+    header("HTTP/1.0 404 Not Found");
+?>
+<!DOCTYPE html>
+<html>
+<head><title>404 Not Found</title></head>
+<body bgcolor="white">
+<center><h1>404 Not Found</h1></center>
+<hr><center>nginx/1.2.1</center>
+</body>
+</html>
+<?php
+    exit();
+  }
+?>
