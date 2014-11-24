30b789172d24507254cfc977e0e8401493a5ffad
BrainStone <yannick@tedworld.de>
2014-11-24 18:43:22 +0100

    Added some translations

--- includes/menu.php
+++ includes/menu.php
@@ -1,22 +1,22 @@
 <ul id="navigation" class="nav-main">
-    <li><a href="#">Home</a></li>
+    <li><a href="#"><?php echo translate("menu.home"); ?></a></li>
     
-    <li class="list"><a href="#">Seiten</a>
-    <ul class="nav-sub">
-	<li><a href="#">1</a></li>
-	<li><a href="#">2</a></li>
-	<li><a href="#">3</a></li>
-    </ul>
+    <li class="list"><a href="#"><?php echo translate("menu.pages"); ?></a>
+        <ul class="nav-sub">
+            <li><a href="#">1</a></li>
+            <li><a href="#">2</a></li>
+            <li><a href="#">3</a></li>
+        </ul>
     </li>
     
-    <li class="list"><a href="#">About</a>
-    <ul class="nav-sub">
-	<li><a href="#">About</a></li>
-	<li><a href="#">Commercial Use</a></li>
-	<li><a href="#">Terms Of Use</a></li>
-    </ul>
+    <li class="list"><a href="#"><?php echo translate("menu.about"); ?></a>
+        <ul class="nav-sub">
+            <li><a href="#"><?php echo translate("menu.about.about"); ?></a></li>
+            <li><a href="#"><?php echo translate("menu.about.commercial"); ?></a></li>
+            <li><a href="#"><?php echo translate("menu.about.tos"); ?></a></li>
+            <li><a href="#"><?php echo translate("menu.about.stats"); ?></a></li>
+        </ul>
     </li>
-    <li><a href="#">Contact</a></li>
-    
 
+    <li><a href="#"><?php echo translate("menu.contact"); ?>/a></li>
 </ul>
