37932142d439634473a995630539faed3c0c7a98
BrainStone <yannick@tedworld.de>
2015-05-23 20:39:29 +0200

    Fixed "Last Modified"-Header

--- compress.php
+++ compress.php
@@ -18,9 +18,10 @@
   function setHeaders() {
     global $file;
 
+    $tsstring = gmdate("D, d M Y H:i:s \G\M\T", filemtime($file));
     $if_modified_since = isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) ? $_SERVER['HTTP_IF_MODIFIED_SINCE'] : false;
 
-    if($if_modified_since && ($if_modified_since == gmdate("D, d M Y H:i:s \G\M\T", filemtime($file)))) {
+    if($if_modified_since && ($if_modified_since == $tsstring)) {
       header('HTTP/1.1 304 Not Modified');
       exit();
     } else {
