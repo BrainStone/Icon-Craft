173dcd0bbf5c2497cae2334baaeb4b5faa5dcdee
BrainStone <yannick@tedworld.de>
2015-05-25 15:00:30 +0200

    Fixed cronjob
    - Added required path to PATH

--- optimize_cache.sh
+++ optimize_cache.sh
@@ -11,6 +11,7 @@ fi
 
 dir="/var/www/icon-craft/cache/render/"
 files=($(find $dir -iname "*png" | sort))
+PATH="$PATH:/usr/local/bin"
 
 function imageOptimizer() {
   echo ${files[@]} | xargs -r --max-procs=4 -n1 sh -c 'echo -e "Optimizing\t\t${1:'${#dir}'} ..." && (optipng -o7 "$1" && advpng -z -4 "$1" && advdef -z -4 "$1" && pngcrush "$1" "$1.optimized" && rm "$1") > /dev/null && echo -e "Finished optimizing\t${1:'${#dir}'}"' -
