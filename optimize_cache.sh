86d2d2d5e32c1df121cbaa5b895bdf7f86188da8
BrainStone <yannick@tedworld.de>
2015-05-25 02:23:30 +0200

    Optimized script for cron-job
    - No more fancy time measuring

--- optimize_cache.sh
+++ optimize_cache.sh
@@ -11,9 +11,6 @@ fi
 
 dir="/var/www/icon-craft/cache/render/"
 files=($(find $dir -iname "*png" | sort))
-scriptdir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
-source "$scriptdir/executeWithTime.sh"
 
 function imageOptimizer() {
   echo ${files[@]} | xargs -r --max-procs=4 -n1 sh -c 'echo -e "Optimizing\t\t${1:'${#dir}'} ..." && (optipng -o7 "$1" && advpng -z -4 "$1" && advdef -z -4 "$1" && pngcrush "$1" "$1.optimized" && rm "$1") > /dev/null && echo -e "Finished optimizing\t${1:'${#dir}'}"' -
@@ -28,7 +25,7 @@ then
   printf -- '%s\n' "${files[@]}"
   echo
   
-  executeWithTime "Image Optimizer" imageOptimizer
+  time imageOptimizer
   
   files=("${files[@]/%/.optimized}")
   size=$(du -bsc ${files[@]} | tail -1 | cut -f1)
