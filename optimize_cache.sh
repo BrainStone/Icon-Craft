b2f6c46a99166e90cedfdbe22644851858aa782c
BrainStone <yannick@tedworld.de>
2015-11-11 23:15:23 +0100

    Fixed Cache Optimizer

--- optimize_cache.sh
+++ optimize_cache.sh
@@ -13,8 +13,12 @@ dir="/var/www/icon-craft/cache/render/"
 files=($(find $dir -iname "*png" | sort))
 PATH="$PATH:/usr/local/bin"
 
+if [ -t 0 ]; then
+  interactive=1
+fi
+
 function imageOptimizer() {
-  echo ${files[@]} | xargs -r --max-procs=4 -n1 sh -c 'if [ -n "$PS1" ]; then echo -e "Optimizing\t\t${1:'${#dir}'} ..."; fi && (optipng -o7 "$1" && advpng -z -4 "$1" && advdef -z -4 "$1" && pngcrush "$1" "$1.optimized" && rm "$1") > /dev/null && if [ -n "$PS1" ]; then echo -e "Finished optimizing\t${1:'${#dir}'}"; fi' -
+  echo ${files[@]} | xargs -r --max-procs=4 -n1 sh -c 'if [ -n "'$interactive'" ]; then echo -e "Optimizing\t\t${1:'${#dir}'} ..."; fi && (optipng -o7 "$1" && advpng -z -4 "$1" && advdef -z -4 "$1" && pngcrush "$1" "$1.optimized" && rm "$1") > /dev/null 2>&1 && if [ -n "'$interactive'" ]; then echo -e "Finished optimizing\t${1:'${#dir}'}"; fi' -
 }
 
 source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/humanReadable.sh"
@@ -23,16 +27,16 @@ if [ -n "$files" ]
 then
   oldsize=$(du -bsc ${files[@]} | tail -1 | cut -f1)
   numfiles=${#files[*]}
-  
+
   echo "Files to be optimized:"
   printf -- '%s\n' "${files[@]}"
   echo
-  
+
   time imageOptimizer
-  
+
   files=("${files[@]/%/.optimized}")
   size=$(du -bsc ${files[@]} | tail -1 | cut -f1)
   permille=$((1000 - ((size * 1000) / oldsize)))
-  
+
   echo -e "\n\n\nFiles:\t\t$numfiles\nOld Size:\t$(humanReadable $oldsize)\nNew Size:\t$(humanReadable $size)\nReduced by:\t$(humanReadable $(($oldsize - $size))) ($((permille / 10)).$((permille % 10))%)"
 fi
